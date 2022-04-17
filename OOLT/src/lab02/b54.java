5fa7cc00fcdc0b2973a90858b118899bb10eb1bf
haiduong2k1 <haiduongfc2001@gmail.com>
2022-04-17 20:37:30 +0700

    Lab02

---
+++ OOLT/src/lab02/b54.java
@@ -0,0 +1,21 @@
+package lab02;
+
+import java.util.Scanner;
+
+public class b54 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập n: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+        System.exit(0);
+    }
+}
